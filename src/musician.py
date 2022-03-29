75805953b8a542f10e50bac3657e471fe57991c7
crabbit-git <98990717+crabbit-git@users.noreply.github.com>
2022-03-29 11:43:55 +0100

    Musician tests pass

---
+++ src/musician.py
@@ -0,0 +1,14 @@
+class Musician:
+    def __init__(self, name, salary):
+        self.name = name
+        self.salary = salary
+        self.bank = 0
+    
+    def get_salary(self):
+        return self.salary
+    
+    def get_bank(self):
+        return self.bank
+    
+    def add_money(self, salary):
+        self.bank += salary
